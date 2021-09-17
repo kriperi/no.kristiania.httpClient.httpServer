d15ece0743d66334e9d40fa86f5fe329976b4d8f
kriperi <mikkelfs@gmail.com>
2021-09-17 09:56:08 +0200

    Test 3 should return red

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -13,6 +13,6 @@ public class HttpClientTest {
 
     @Test
     void shouldReturnStatusCode () {
-        Assertions.assertEquals(200, new HttpClient("httpbin.org", 80, "/html").getStatusCode());
+        assertEquals(404, new HttpClient("httpbin.org", 80, "/no-such-page").getStatusCode());
     }
 }

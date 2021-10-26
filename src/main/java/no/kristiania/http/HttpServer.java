8bd9ad43f097e9ba3429cec7ce99c47dc987a580
kriperi <mikkelfs@gmail.com>
2021-10-26 14:39:01 +0200

    refactor

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -43,25 +43,7 @@ public class HttpServer {
     }
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-
-        Socket clientSocket = serverSocket.accept();
-
-        String requestLine = HttpClient.readLine(clientSocket);
-
-        System.out.println(requestLine);
-
-        String html = "<p>Hallååå</p>";
-        String contentType = "text/html; charset=utf-8";
-
-        String response = "HTTP/1.1 200 OK \r\n" +
-                "Content-Length: " + html.getBytes().length + "\r\n" +
-                "Content-Type: " + contentType + "\r\n" +
-                "Connection: Close\r\n" +
-                "\r\n"  +
-                html;
-
-        clientSocket.getOutputStream().write(response.getBytes());
+        new HttpServer (1962);
 
     }
 
--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -26,8 +26,9 @@ class HttpServerTest {
     void shouldRespondWith200ForUnknownRequestTarget() throws IOException {
         HttpServer server = new HttpServer(10003);
         HttpClient client = new HttpClient("localhost", server.getPort(), "/hello");
-        assertEquals(200, client.getStatusCode());
-        assertEquals("Hello world", client.getMessageBody());
-
+        assertAll(
+                () -> assertEquals(200, client.getStatusCode()),
+                () -> assertEquals("Hello world", client.getMessageBody())
+        );
     }
 }
